03ff359385791681366465852693032347e8d9a5
Neverlabs <217960416+neveerlabs@users.noreply.github.com>
2025-12-25 21:48:43 +0700

    Fix repository URL in VERSION constant

--- tocket/constants.py
+++ tocket/constants.py
@@ -1,9 +1,8 @@
-# File: tocket/constants.py
 import os
 from pathlib import Path
 
 APPNAME = "Tocket"
-VERSION = "Tocket-Core - v4.2.0.1 (c) Dec 2025    https://github.com/neveerlabs/"
+VERSION = "Tocket-Core - v4.2.0.1 (c) Dec 2025    https://github.com/neveerlabs/Tocket.git"
 
 DB_DIR = Path.home() / f".{APPNAME.lower()}"
 DB_FILE = DB_DIR / "tocket.db"
