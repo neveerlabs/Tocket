7766c6acb1f8a22f9064de1dff0f4d3dfb6be514
M. Syalman Al Farizi <217960416+neveerlabs@users.noreply.github.com>
2026-08-20 21:33:39 +0700

    Delete tocket directory

--- tocket/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-APPNAME = "Tocket"
-VERSION = "Tocket-Core - v4.2.0.1 (c) Dec 2025    https://github.com/neveerlabs/Tocket.git"
-
-DB_DIR = Path.home() / f".{APPNAME.lower()}"
-DB_FILE = DB_DIR / "tocket.db"
